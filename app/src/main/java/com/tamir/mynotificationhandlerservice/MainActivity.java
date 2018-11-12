428f36655a2e0ec715f5d680ed0269b561df1bbf
ZedLeppelin <tamir.nahum@mail.huji.ac.il>
2018-11-12 12:45:45 +0200

    toast working

--- app/src/main/java/com/tamir/mynotificationhandlerservice/MainActivity.java
+++ app/src/main/java/com/tamir/mynotificationhandlerservice/MainActivity.java
@@ -1,43 +1,74 @@
 package com.tamir.mynotificationhandlerservice;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Calendar;
+
 public class MainActivity extends Activity {
 
+    static String CHANNEL_ID = "10";
     private TextView txtView;
-    private NotificationReceiver nReceiver;
+    public NotificationManager notificationManager;
+    public NotificationChannel mNotificationChannel;
+
+    private void createChannel(Context context) {
+
+        notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        CharSequence channelName = "default Channel";
+        int importance = NotificationManager.IMPORTANCE_LOW;
+        this.mNotificationChannel = new NotificationChannel(CHANNEL_ID, channelName, importance);
+        mNotificationChannel.enableLights(true);
+        mNotificationChannel.setLightColor(Color.MAGENTA);
+//        making sure the notification won't activate the vibration
+        mNotificationChannel.enableVibration(false);
+        notificationManager.createNotificationChannel(mNotificationChannel);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createChannel(this);
         txtView = (TextView) findViewById(R.id.textView);
-        nReceiver = new NotificationReceiver();
         IntentFilter filter = new IntentFilter();
-        filter.addAction("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
-        registerReceiver(nReceiver, filter);
+        filter.addAction("com.tamir.mynotificationhandlerservice.NOTIFICATION_LISTENER_EXAMPLE");
+        Intent myIntent = new Intent(this, MyHandlerService.class);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        PendingIntent pendingIntent = PendingIntent.getService(this, 0, myIntent, 0);
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.SECOND, 1);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.add(Calendar.DAY_OF_MONTH, 0);
+
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis() + 30000, 10000, pendingIntent);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        unregisterReceiver(nReceiver);
     }
 
     public void buttonClicked(View v) {
 
         if (v.getId() == R.id.btnCreateNotify) {
             NotificationManager nManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-            NotificationCompat.Builder ncomp = new NotificationCompat.Builder(this);
+            NotificationCompat.Builder ncomp = new NotificationCompat.Builder(this, mNotificationChannel.getId());
             ncomp.setContentTitle("My Notification");
             ncomp.setContentText("Notification Listener Service Example");
             ncomp.setTicker("Notification Listener Service Example");
--- app/src/main/java/com/tamir/mynotificationhandlerservice/MyHandlerService.java
+++ app/src/main/java/com/tamir/mynotificationhandlerservice/MyHandlerService.java
@@ -4,27 +4,92 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Handler;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MyHandlerService extends NotificationListenerService {
 
+    // constant
+    public static final long NOTIFY_INTERVAL =  10 * 1000;
+    private Timer mTimer = null;
+    private Handler mHandler = new Handler();
+
     private String TAG = this.getClass().getSimpleName();
-    private NLServiceReceiver nlservicereciver;
     @Override
     public void onCreate() {
         super.onCreate();
-        nlservicereciver = new NLServiceReceiver();
         IntentFilter filter = new IntentFilter();
-        filter.addAction("com.kpbird.nlsexample.NOTIFICATION_LISTENER_SERVICE_EXAMPLE");
-        registerReceiver(nlservicereciver,filter);
+        filter.addAction("com.tamir.mynotificationhandlerservice.NOTIFICATION_LISTENER_SERVICE_EXAMPLE");
+//        registerReceiver(mMessageReceiver,filter);
+
+        // cancel if already existed
+        if (mTimer != null) {
+            mTimer.cancel();
+        } else {
+            // recreate new
+            mTimer = new Timer();
+        }
+        registerReceiver(mMessageReceiver, new IntentFilter("end_time"));
+        // schedule task
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, 2018);
+        cal.set(Calendar.MONTH, 11);
+        Date date = new Date(2018, 11, 12, 12, 42);
+        mTimer.scheduleAtFixedRate(new TimeDisplayTimerTask(), 30000, NOTIFY_INTERVAL);
     }
+
+    class TimeDisplayTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+            // run on another thread
+            mHandler.post(new Runnable() {
+
+                @Override
+                public void run() {
+                    // display toast
+
+                    SimpleDateFormat dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm aa");
+                    String currentDateTime = dateTimeFormat.format(new Date()).toString();
+                    String[] startSplit = currentDateTime.split(" ");
+                    String CurrentTime = startSplit[1] + " " + startSplit[2].toUpperCase();
+
+                    Intent intent = new Intent();
+                    intent.putExtra("timer", "1");
+                    Toast toast = Toast.makeText(getApplicationContext(),"this is a toast 4", Toast.LENGTH_SHORT);
+                    toast.show();
+                    sendBroadcast(intent);
+
+                }
+
+            });
+        }
+
+    }
+    // handler for received Intents for the "my-event" event
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Extract data included in the Intent
+            if (mTimer != null)
+                mTimer.cancel();
+            unregisterReceiver(mMessageReceiver);
+        }
+    };
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        unregisterReceiver(nlservicereciver);
+        unregisterReceiver(mMessageReceiver);
     }
 
     @Override
@@ -32,7 +97,7 @@ public class MyHandlerService extends NotificationListenerService {
 
         Log.i(TAG,"**********  onNotificationPosted");
         Log.i(TAG,"ID :" + sbn.getId() + "t" + sbn.getNotification().tickerText + "t" + sbn.getPackageName());
-        Intent i = new  Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
+        Intent i = new  Intent("com.tamir.mynotificationhandlerservice.NOTIFICATION_LISTENER_EXAMPLE");
         i.putExtra("notification_event","onNotificationPosted :" + sbn.getPackageName() + "n");
         sendBroadcast(i);
 
@@ -42,7 +107,7 @@ public class MyHandlerService extends NotificationListenerService {
     public void onNotificationRemoved(StatusBarNotification sbn) {
         Log.i(TAG,"********** onNOtificationRemoved");
         Log.i(TAG,"ID :" + sbn.getId() + "t" + sbn.getNotification().tickerText +"t" + sbn.getPackageName());
-        Intent i = new  Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
+        Intent i = new  Intent("com.tamir.mynotificationhandlerservice.NOTIFICATION_LISTENER_EXAMPLE");
         i.putExtra("notification_event","onNotificationRemoved :" + sbn.getPackageName() + "n");
 
         sendBroadcast(i);
@@ -56,17 +121,17 @@ public class MyHandlerService extends NotificationListenerService {
                 MyHandlerService.this.cancelAllNotifications();
             }
             else if(intent.getStringExtra("command").equals("list")){
-                Intent i1 = new  Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
+                Intent i1 = new  Intent("com.tamir.mynotificationhandlerservice.NOTIFICATION_LISTENER_EXAMPLE");
                 i1.putExtra("notification_event","=====================");
                 sendBroadcast(i1);
                 int i=1;
                 for (StatusBarNotification sbn : MyHandlerService.this.getActiveNotifications()) {
-                    Intent i2 = new  Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
+                    Intent i2 = new  Intent("com.tamir.mynotificationhandlerservice.NOTIFICATION_LISTENER_EXAMPLE");
                     i2.putExtra("notification_event",i +" " + sbn.getPackageName() + "n");
                     sendBroadcast(i2);
                     i++;
                 }
-                Intent i3 = new  Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
+                Intent i3 = new  Intent("com.tamir.mynotificationhandlerservice.NOTIFICATION_LISTENER_EXAMPLE");
                 i3.putExtra("notification_event","===== Notification List ====");
                 sendBroadcast(i3);
 
